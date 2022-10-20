8d4127e8d223f4c729a001658811722bd71835e3
siavash sepahi <sepahisiavash@gmail.com>
2022-10-20 18:28:48 +0330

    closing path env

--- scripts/airsim_env.py
+++ scripts/airsim_env.py
@@ -7,3 +7,7 @@ def open_airsim_train_env():
 
 def open_airsim_test_env():
     subprocess.Popen('cmd /k "cd TestEnv && start TestEnv.exe"', shell=True)
+
+
+def close_airsim_env():
+    subprocess.call("TASKKILL /F /IM Trainenv.exe", shell=True)
