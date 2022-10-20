a8cb50685a1ad8fed7c43940f429e52dbc89952c
siavash sepahi <sepahisiavash@gmail.com>
2022-10-20 17:48:18 +0330

    fixing opening train and test env

--- scripts/airsim_env.py
+++ scripts/airsim_env.py
@@ -2,8 +2,8 @@ import subprocess
 
 
 def open_airsim_train_env():
-    subprocess.Popen('cmd /k "start TrainEnv/Trainenv.exe"', shell=True)
+    subprocess.Popen('cmd /k "cd TrainEnv && Trainenv.exe"', shell=True)
 
 
 def open_airsim_test_env():
-    subprocess.Popen('cmd /k "start TestEnv/TestEnv.exe"', shell=True)
+    subprocess.Popen('cmd /k "cd TestEnv && start TestEnv.exe"', shell=True)
